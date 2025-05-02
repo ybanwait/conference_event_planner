1acadc23edaaf39bbdfdb418f3f103c1b1414412
yadwinder <yadwinder.banwait@gmail.com>
2025-05-02 18:14:34 -0400

    Added event planning code

--- src/TotalCost.jsx
+++ src/TotalCost.jsx
@@ -3,7 +3,7 @@ import "./TotalCost.css";
 
 const TotalCost = ({ totalCosts, ItemsDisplay }) => {
   
-
+const total_amount = totalCosts.venue + totalCosts.av + totalCosts.meals;
   return (
     <div className="pricing-app">
       <div className="display_box">
@@ -11,12 +11,12 @@ const TotalCost = ({ totalCosts, ItemsDisplay }) => {
           <p className="preheading"><h3>Total cost for the event</h3></p>
         </div>
         <div>
-          <h2 id="pre_fee_cost_display" className="price">
-           
-          </h2>
+            <h2 id="pre_fee_cost_display" className="price">
+                ${total_amount}
+            </h2>
          
-            <div>
-             
+            <div className="render_items">
+                <ItemsDisplay />
             </div>
         </div>
       </div>
